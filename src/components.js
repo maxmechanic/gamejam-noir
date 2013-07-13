9c9ab322fe7ee9c073cfaf1ce66a514a65e92261
Max <max@teamatb.com>
2013-07-12 23:38:18 -0400

    JS scaffolding.

---
+++ src/components.js
@@ -0,0 +1,6 @@
+
+Crafty.c('Player', {});
+Crafty.c('Inventory', {});
+Crafty.c('Item', {}); //Have one component for when on level and in inventory?
+Crafty.c('DialogBox', {});
+Crafty.c('DialogChoice', {}); //Inherets from DialogBox
