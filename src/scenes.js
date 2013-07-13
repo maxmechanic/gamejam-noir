16820b241016706adc6fcca7a70e3f2561914a4c
Max <max@teamatb.com>
2013-07-13 15:19:10 -0400

    Given an array of vectors, player will go to a position near what's clicked.

---
+++ src/scenes.js
@@ -0,0 +1,48 @@
+Crafty.scene('Intro', function () {
+});
+Crafty.scene('Loading', function() {});
+
+//For playable scenes, define barriers of the background for what can be considered the floor.
+Crafty.scene('Office', function() {
+    console.log('office scene launched');
+    Crafty.background("url('./assets/beachbg.png')");
+    var player = Crafty.e('Player')
+        .attr({ x: 580, y: 100, w: 10, h: 100 });
+
+    // var box = new Crafty.polygon([0,0], [500,0], [500,500], [0,500]);
+    // var itemBox = new Crafty.polygon([0,0],[0,500],[500,500],[500,0]);
+        // var player = Crafty.e("2D");
+        //     player.onMouseDown = function(e) {
+        //         alert();
+        //     };
+    var box = Crafty.e("2D,DOM,Collision,WiredHitBox,Solid,Mouse").attr({w:500,h:500,x:0,y:0});
+
+    var positions = [];
+    positions.push(new Crafty.math.Vector2D(50,50));
+    positions.push(new Crafty.math.Vector2D(200,200));
+    positions.push(new Crafty.math.Vector2D(500,500));
+    positions.push(new Crafty.math.Vector2D(1000,50));
+
+    Crafty.e("2D, DOM, Color").attr({w:10,h:10,x:50,y:50}).color('green');
+    Crafty.e("2D, DOM, Color").attr({w:10,h:10,x:200,y:200}).color('green');
+    Crafty.e("2D, DOM, Color").attr({w:10,h:10,x:500,y:500}).color('green');
+    Crafty.e("2D, DOM, Color").attr({w:10,h:10,x:1000,y:50}).color('green');
+
+
+
+    Crafty.addEvent(player, Crafty.stage.elem, "mousedown", checkClick);
+
+    function checkClick(e) {
+        var vec = new Crafty.math.Vector2D(e.realX,e.realY);
+        var newPos = 0;
+        for (var i = 1; i < positions.length; i++) {
+            if (vec.distance(positions[i]) < vec.distance(positions[newPos])) {
+                newPos = i;
+            }
+        }
+
+        player.moveChar(positions[newPos].x, positions[newPos].y);
+    }
+
+});
+Crafty.scene('Inventory', function() {});
