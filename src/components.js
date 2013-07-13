81b5cebda6528b5da12f6a88c1705bfb83416e12
Max <max@teamatb.com>
2013-07-13 15:18:55 -0400

    Basically just the player working.

--- src/components.js
+++ src/components.js
@@ -1,6 +1,51 @@
 
-Crafty.c('Player', {});
-Crafty.c('Inventory', {});
-Crafty.c('Item', {}); //Have one component for when on level and in inventory?
-Crafty.c('DialogBox', {});
-Crafty.c('DialogChoice', {}); //Inherets from DialogBox
+Crafty.c('Player', {
+    init: function() {
+        this.requires('2D, DOM, Mouse, Color, Tween')
+        .color('rgb(20, 75, 40)').stopOnSolids();
+
+
+    },
+
+    engageItem: function(item) {
+        alert(item);
+    },
+
+    moveChar: function(x, y, item) {
+
+        this.tween({x:x, y:y}, 200);
+
+    }
+
+});
+
+
+Crafty.c('Inventory', {
+    init: function() {
+
+    }
+});
+
+Crafty.c('Item', {
+    init: function() {
+        this.requires('2D, Grid, Mouse');
+    },
+
+}); //Have one component for when on level and in inventory?
+
+Crafty.c('UseableItem', {
+    init: function() {
+        this.requires('Item');
+    }
+});
+
+Crafty.c('DialogBox', {
+    init: function() {
+
+    }
+});
+Crafty.c('DialogChoice', {
+    init: function() {
+
+    }
+}); //Inherits from DialogBox
